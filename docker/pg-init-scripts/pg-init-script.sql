adec68d94e6dc2066c03c37700104d2cb9d2046d
Adelbas <dina.adel@yandex.ru>
2024-03-11 19:40:47 +0300

    feat: add payment-gate and merchant-provider to docker

--- docker/pg-init-scripts/pg-init-script.sql
+++ docker/pg-init-scripts/pg-init-script.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE fintech_product_engine;
 CREATE DATABASE fintech_origination;
 CREATE DATABASE fintech_api;
+CREATE DATABASE fintech_payment_gate;
+CREATE DATABASE fintech_merchant_provider;
